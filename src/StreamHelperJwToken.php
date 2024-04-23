42281358919fa02c7802777cfb5d735cd1e4a56b
omegaalfa <omegaalfa.wr3@gmail.com>
2024-04-23 17:00:29 -0300

    Create StreamHelperJwToken.php

---
+++ src/StreamHelperJwToken.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace omegalfa\jwtoken;
+
+use Psr\Http\Message\StreamInterface;
+use src\stream\Stream;
+
+trait StreamHelperJwToken
+{
+
+	/**
+	 * @param  string  $file
+	 *
+	 * @return Stream
+	 */
+	private function instanceStream(string $file): StreamInterface
+	{
+		return new Stream($file);
+	}
+
+	/**
+	 * @param  string  $file
+	 *
+	 * @return string
+	 */
+	protected function readFile(string $file): string
+	{
+		if(!file_exists($file)) {
+			throw new \InvalidArgumentException("File {$file} does not exist or the path is invalid.");
+		}
+
+		return $this->instanceStream($file)->getContents();
+	}
+}
